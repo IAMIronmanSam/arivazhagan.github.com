bb50d32b2b9ed4cd0ae7dd0219edc2088d909dfb
Arivazhagan Ambigapathi <arivu86@gmail.com>
2013-11-08 14:18:39 +0530

    Changed colors

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
  $(document).ready(function() {
     //$("#contact").modal();
     $.fn.fullpage({
-    	slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage'],
+    	slidesColor: ['#335add', '#f25764', '#66b76d', '#fcc349', '#FFFFFF'],
+		anchors: ['Home', 'Expert', 'MyStuff', 'Contact'],
 		menu: '#top_nav'});
   });
