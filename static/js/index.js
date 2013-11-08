b545fc535c6bca9992c801c11ff2d7b6aa0b5437
Arivazhagan Ambigapathi <arivu86@gmail.com>
2013-11-08 15:08:21 +0530

    Added Gradient

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
  $(document).ready(function() {
     //$("#contact").modal();
     $.fn.fullpage({
-    	slidesColor: ['#335add', '#f25764', '#66b76d', '#fcc349', '#FFFFFF'],
+    	slidesColor: ['#FFFFFF', '#f25764', '#66b76d', '#fcc349', '#335add'],
 		anchors: ['Home', 'Expert', 'MyStuff', 'Contact'],
 		menu: '#top_nav'});
   });
