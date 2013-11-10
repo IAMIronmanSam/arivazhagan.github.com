2951ecae619f22600c1b40138b7e223378e375c4
Arivazhagan <arivu86@gmail.com>
2013-11-10 12:11:17 +0530

    My Blinks

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
  $(document).ready(function() {
     //$("#contact").modal();
     $.fn.fullpage({
-    	slidesColor: ['#FFFFFF', '#f25764', '#66b76d', '#fcc349', '#335add'],
-		anchors: ['Home', 'Expert', 'MyStuff', 'Contact'],
+    	slidesColor: ['#FFFFFF', '#f25764', '#66b76d', '#000000', '#fcc349'],
+		anchors: ['Home', 'Expert', 'MyStuff','Blink', 'Contact'],
 		menu: '#top_nav'});
   });
