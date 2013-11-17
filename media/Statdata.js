d11bd735ee3eb33da844f48c882497a4de3aa02c
Arivazhagan <arivu86@gmail.com>
2013-11-17 13:17:17 +0530

    Formating

---
+++ media/Statdata.js
@@ -0,0 +1,42 @@
+//<li data-menuanchor="Home" ><a href = "#Home">Home</a></li>
+//                <li data-menuanchor="Expert" ><a href = "#Expert">Expertise</a></li>
+//                <li data-menuanchor="MyStuff" ><a href = "#MyStuff">My Stuff</a></li>
+//                <li data-menuanchor="Blink" ><a href = "#Blink" >My Blinks</a></li>
+//                <li data-menuanchor="Contact" ><a href = "#Contact" >Contact</a></li>
+//<h2><ul>
+//                        <li>Markup : HTML5,CSS3</li>
+//                        <li>Script: Javascript,Coffee Script</li>
+//                        <li>Technology: Sharepoint,Java</li>
+//                        <li>Tools: Fiddler,Visual Studio,Netbeans,Eclipse</li>
+////                    </ul></h2>
+////<center> <h2> <i>“Good Artists Copy,<br> Great Artists Steal” <br> - Steve Jobs</i></h2></center>
+//<h3><ul class="mystuff">
+//                        <li>Watch Tech discussion</li>
+//                        <li>Always playing with Javascript,Coffee Script</li>
+//                        <li>My roots from JAVA so basically OOPS Guy</li>
+//                        <li>Ubuntu Lover</li>
+//                        <li>#PERFMATTERS</li>
+//                        <li>Intrested in HTML5,CSS3</li>
+//                        <li>Hardcore bug creator</li>
+//                        <li>Open source Contribution</li>
+//                        <li>Read only dan brown books</li>
+//                        <li>Watch lot of Movies!!</li>
+//                    </ul></h3>
+//            </div>
+//             <div class = "col-md-3">
+//                <center> <h2> <i>“Work hard in silence,let your success be your noise.-Oprah” </i></h2></center>
+//            </div>
+var globalNav,expertise,quotes,stuffs ={};
+globalNav = {"Home","Expertise","My Stuff","Blink","Contact"};
+expertise = {"Markup":"HTML,CSS3","Script":"Javascript,Coffescript","Technology":"Sharepoint,Java","Tools":"Fiddler,Visual Studio,Netbeans,Eclipse"};
+stuffs = {"Watch Tech discussion",
+        "Always playing with Javascript,Coffee Script",
+        "My roots from JAVA so basically OOPS Guy",
+        "Ubuntu Lover",
+        "#PERFMATTERS",
+        "Intrested in HTML5,CSS3",
+        "Hardcore bug creator",
+        "Open source Contribution",
+        "Read only dan brown books",
+        "Watch lot of Movies!!"};
+quotes = {"Good Artists Copy,Great Artists Steal- Steve Jobs","Work hard in silence,let your success be your noise.-Oprah"};
